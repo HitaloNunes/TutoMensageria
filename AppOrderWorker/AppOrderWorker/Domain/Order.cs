96fabe2deddc783fa1866f9475d7dfb669ec172c
Hitalo Wingred <38959476+TioPatinhas00@users.noreply.github.com>
2022-07-11 17:37:37 -0300

    Consumer

---
+++ AppOrderWorker/AppOrderWorker/Domain/Order.cs
@@ -0,0 +1,9 @@
+﻿namespace AppOrderWorker.Domain
+{
+    public class Order
+    {
+        public int OrderNumber { get; set; }
+        public string ItemName { get; set; }
+        public float Price { get; set; }
+    }
+}
---
+++ AppOrderWorker/AppOrderWorker/Program.cs
@@ -0,0 +1,39 @@
+﻿using AppOrderWorker.Domain;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Text.Json;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare(queue: "orderQueue",
+                         durable: false,
+                         exclusive: false,
+                         autoDelete: false,
+                         arguments: null);
+
+    var consumer = new EventingBasicConsumer(channel);
+    consumer.Received += (model, ea) =>
+    {
+        try
+        {
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+
+            var order = JsonSerializer.Deserialize<Order>(message);
+            Console.WriteLine($"Order: {order.OrderNumber}|{order.ItemName}|{order.Price}");
+            channel.BasicAck(ea.DeliveryTag, false);
+        } catch (Exception ex)
+        {
+            channel.BasicNack(ea.DeliveryTag, false, true);
+        }
+    };
+    channel.BasicConsume(queue: "orderQueue",
+                         autoAck: false,
+                         consumer: consumer);
+
+    Console.WriteLine(" Press [enter] to exit.");
+    Console.ReadLine();
+}
